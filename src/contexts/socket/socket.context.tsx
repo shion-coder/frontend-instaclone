1fae3535b2c0c2b77c8fbb027382e3b9694633b0
Shion <shion.coder@gmail.com>
2020-08-24 20:57:14 +0700

    Update socket context

--- src/contexts/socket/socket.context.tsx
+++ src/contexts/socket/socket.context.tsx
@@ -1,7 +1,9 @@
 import React, { FC, createContext, useEffect, useState, useContext } from 'react';
-import socketIo from 'socket.io-client';
+import socketIo, { Socket } from 'socket.io-client';
+import { useSelector } from 'react-redux';
 
 import { API_URL } from 'config';
+import { RootStateProps } from 'store';
 
 /* -------------------------------------------------------------------------- */
 
@@ -9,16 +11,25 @@ const SocketContext = createContext<SocketIOClient.Socket | null>(null);
 
 export const SocketProvider: FC = ({ children }) => {
   const [io, setIo] = useState<SocketIOClient.Socket | null>(null);
+  const token = useSelector((state: RootStateProps) => state.auth.token);
 
   useEffect(() => {
-    const socket = socketIo(`${API_URL}`);
+    let socket: SocketIOClient.Socket;
+
+    if (!token) {
+      socket = socketIo(API_URL);
+    } else {
+      socket = socketIo(API_URL, {
+        query: { token },
+      });
+    }
 
     setIo(socket);
 
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [token]);
 
   return <SocketContext.Provider value={io}>{children}</SocketContext.Provider>;
 };
