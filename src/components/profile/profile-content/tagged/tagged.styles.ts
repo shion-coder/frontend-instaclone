3d1059572dcb628a6353a0f3bbcbe8a76e00541b
Shion <shion.coder@gmail.com>
2020-09-14 02:50:47 +0700

    Update min-height in post content to prevent error with image lay below

--- src/components/profile/profile-content/tagged/tagged.styles.ts
+++ src/components/profile/profile-content/tagged/tagged.styles.ts
@@ -5,6 +5,7 @@ import { Grid } from '@material-ui/core';
 
 export const Content = styled.div`
   padding-top: ${({ theme }) => theme.material.spacing(5) + 'px'};
+  min-height: 330px;
 `;
 
 export const Load = styled(Grid)`
