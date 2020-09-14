1142ebf63f79387f21f62e9a1c6f75185e10dec5
Shion <shion.coder@gmail.com>
2020-09-15 01:57:24 +0700

    Update loading skeleton in mobile interface

--- src/components/profile/profile-content/posts/posts.loading.tsx
+++ src/components/profile/profile-content/posts/posts.loading.tsx
@@ -1,37 +1,50 @@
 import React, { FC } from 'react';
+import { useTheme, useMediaQuery } from '@material-ui/core';
 
 import { PostLoading } from './post';
 
 /* -------------------------------------------------------------------------- */
 
-export const PostsLoading: FC = () => (
-  <>
-    <PostLoading />
+export const PostsLoading: FC = () => {
+  const matchesXS = useMediaQuery(useTheme().breakpoints.down('xs'));
 
-    <PostLoading />
+  return (
+    <>
+      <PostLoading />
 
-    <PostLoading />
-  </>
-);
+      <PostLoading />
 
-export const Loading: FC = () => (
-  <>
-    <PostLoading />
+      <PostLoading />
 
-    <PostLoading />
+      {matchesXS && <PostLoading />}
+    </>
+  );
+};
 
-    <PostLoading />
+export const Loading: FC = () => {
+  const matchesXS = useMediaQuery(useTheme().breakpoints.down('xs'));
 
-    <PostLoading />
+  return (
+    <>
+      <PostLoading />
 
-    <PostLoading />
+      <PostLoading />
 
-    <PostLoading />
+      <PostLoading />
 
-    <PostLoading />
+      <PostLoading />
 
-    <PostLoading />
+      <PostLoading />
 
-    <PostLoading />
-  </>
-);
+      <PostLoading />
+
+      <PostLoading />
+
+      <PostLoading />
+
+      <PostLoading />
+
+      {matchesXS && <PostLoading />}
+    </>
+  );
+};
